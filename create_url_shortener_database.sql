02ae39bac5d6d2884f4fcd262588d9a34910da1f
bbouley <b.bouley@hotmail.co.uk>
2015-10-23 14:39:40 -0600

    testing gitignore in new file

--- create_url_shortener_database.sql
+++ create_url_shortener_database.sql
@@ -8,23 +8,23 @@ CREATE TABLE urls (
   count INTEGER DEFAULT 0
 );
 
--- INSERT INTO urls (original_url, count) VALUES
--- ('www.google.com', 1),
--- ('www.github.com', 2),
--- ('www.linkedin.com', 3),
--- ('www.facebook.com', 5),
--- ('www.twitter.com', 8);
+ INSERT INTO urls (original_url, count) VALUES
+ ('www.google.com', 1),
+ ('www.github.com', 2),
+ ('www.linkedin.com', 3),
+ ('www.facebook.com', 5),
+ ('www.twitter.com', 8);
 
--- SELECT * FROM urls;
+ SELECT * FROM urls;
 
--- DROP TABLE urls;
+ DROP TABLE urls;
 
--- SELECT * FROM urls;
+ SELECT * FROM urls;
 
--- REVOKE CONNECT ON url_shortener_bradley_bouley FROM public;
+ REVOKE CONNECT ON url_shortener_bradley_bouley FROM public;
 
--- SELECT pg_terminate_backend(pg_stat_activity.pid)
--- FROM pg_stat_activity
--- WHERE pg_stat_activity.datname ='url_shortener_bradley_bouley';
+ SELECT pg_terminate_backend(pg_stat_activity.pid)
+ FROM pg_stat_activity
+ WHERE pg_stat_activity.datname ='url_shortener_bradley_bouley';
 
--- DROP DATABASE url_shortener_bradley_bouley;
+ DROP DATABASE url_shortener_bradley_bouley;
